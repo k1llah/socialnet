faa6f4fa87766e574a672a577759f635faa03f59
John Doe <surenbound@yandex.ru>
2024-06-14 20:08:58 +0300

    install dependencies and setup fsd architecture

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import  autoImport  from '@pinia/nuxt'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      bodyAttrs: {
+        class: "dark:bg-[#0a0a0a]"
+      },
+      
+    },
+  },
+  css: ['@/main.css'],
+  
+  modules: [[
+    '@pinia/nuxt',
+    {
+      autoImports: ['defineStore']
+    }
+  ], "@nuxtjs/tailwindcss", '@formkit/auto-animate', '@vueuse/nuxt', 'nuxt-swiper', "@dargmuesli/nuxt-cookie-control", "nuxt-lodash", "@pinia/nuxt"],
+  pinia: {
+    storesDirs: ['./stores/**', './stores'],
+  },
+  imports: {
+    autoImport: true,
+    dirs: ['./stores/**', '.components/**', '/components/main-components/**',  './components/**/*/*', './components/adminComponents/**/*', './components/adminComponents/**/*', './components/adminComponents/**/*/*/*'],
+  },
+  components: [
+    {
+      path: '~/components', // will get any components nested in let's say /components/test too
+      pathPrefix: false,
+    },
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase:'http://localhost:3001/api'
+    }
+  },
+  ssr:true
 })
---
+++ stores/auth.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+export const useBlog = defineStore("blog", {
+  state: () => ({
+    
+  }),
+  actions: {},
+});
