e2ff19795c2382c550740a5d126c2f7d514a5837
John Doe <surenbound@yandex.ru>
2024-06-14 22:21:53 +0300

    initialization shadcn om project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,39 @@
+const animate = require("tailwindcss-animate")
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+	],
+  prefix: "",
   theme: {
-    extend: {
+    container: {
+      center: true,
+      padding: "2rem",
       screens: {
-        'sm': '320px',
-        'md': '640px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+        "2xl": "1400px",
       },
-      colors:{
-        mainDark: '#09090b',
-				mainBg: '#fff',
-        darkWhite: '#fafafa',
-        darkGray: '#a1a1aa',
-        darkGrayDarken: '#27272a',
-        greenJuice: '#22c55e',
-        redJuice: '#e11d48',
-        darkNightBlue: '#030712',
-        lightGray: '#d1d1d1'
-      }
-    }
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: []
-  
+  plugins: [animate],
 }
